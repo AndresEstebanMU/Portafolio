c567a4c7d8d00d2e1ce0008cde1d8b60b78be47d
Andrés Muñoz <andresestebanmu@gmail.com>
2025-03-31 15:01:23 -0300

    descripcion proyectos

--- src/components/ProjectsImages.jsx
+++ src/components/ProjectsImages.jsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 
 const ProjectsImages = () => {
     return (
-        <div className='flex justify-around items-center @md:flex-col'>
+        <div className='flex md:justify-around md:items-start flex-col md:flex-row items-center'>
             <motion.div 
-            className='flex flex-col justify-center items-center mt-10 gap-5'
+            className='flex flex-col justify-center items-center mt-10 gap-5 md:w-1/3 w-3/4'
             whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}
             >
                 <a href='https://master--voluble-boba-d82bef.netlify.app/'>
@@ -20,9 +20,12 @@ const ProjectsImages = () => {
                 />  
                 </a>
                 <p>Proyecto Restaurant</p>
+                <p className='md:w-3/4 w-fit'>Proyecto desarrollado con React que simula la página de un restaurante. La interfaz se presenta como si el usuario hubiera iniciado sesión con permisos de administrador.</p>
+                {/* <a href="https://github.com/AndresEstebanMU/Pagina-Restaurante">Readme</a> */}
             </motion.div>
+            
             <motion.div 
-            className='flex flex-col justify-center items-center mt-10 gap-5'
+            className='flex flex-col justify-center items-center mt-10 gap-5 md:w-1/3 w-3/4'
             whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}
             >
               <a href='https://andresestebanmu.github.io/Proyecto3-Grafico/'>
@@ -36,9 +39,10 @@ const ProjectsImages = () => {
                 />  
                 </a>
                 <p>Gráfico en base a API</p>
+                <p className='md:w-3/4 w-fit'> Proyecto de visualización de gráficos de películas y series en emisión, utilizando la API de The Movie Database (TMDb). Las calificaciones de cada obra corresponden al promedio de puntuaciones otorgadas por los espectadores.</p>
             </motion.div>
             <motion.div 
-            className='flex flex-col justify-center items-center mt-9  gap-5'
+            className='flex flex-col justify-center items-center mt-10  gap-5 md:w-1/3 w-3/4'
             whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}
             >
                 <a href='https://roaring-tulumba-ba31a3.netlify.app/'>
@@ -46,12 +50,13 @@ const ProjectsImages = () => {
                     className='rounded-xl'
                     src="/botika.png"
                     alt="pagina la botika"
-                    width={410}
-                    height={410}
+                    width={400}
+                    height={400}
                     
                 />   
                 </a>
                 <p>E-Commerce (En Reparación)</p>
+                <p className='md:w-3/4 w-fit'>Proyecto de simulación de una tienda online de bebidas de todo tipo. La plataforma ofrece funciones como registro de usuarios, navegación intuitiva por el catálogo de productos, carrito de compras y un proceso de checkout seguro con pasarela de pagos.</p>
             </motion.div>
         </div>
     )
