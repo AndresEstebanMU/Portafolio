bd270bc836715c3448c34a84f5a82d3cc261d6ff
Andrés Muñoz <andresestebanmu@gmail.com>
2025-12-23 20:13:41 -0300

    curriculum nuevo

--- src/components/TextoHome.jsx
+++ src/components/TextoHome.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 
 const TextoHome = () => {
     const handleDescargar = () => {
-        const url = '/Curriculum-2025-C.pdf'; // Ruta al archivo en el directorio public
+        const url = '/Curriculum Andrés Muñoz Urrutia 2025.pdf'; // Ruta al archivo en el directorio public
         window.open(url);
       };
     
